ba20c441024f0a41a288e8a2be0406cd822275ea
Yulia <peresheina.yulia@yandex.ru>
2023-08-28 12:41:29 +0300

    Seminar 5

---
+++ Sem_5_Iter_and_generator/generator_function.py
@@ -0,0 +1,36 @@
+# Напишите программу, которая выводит
+# на экран числа от 1 до 100.
+# ✔ При этом вместо чисел, кратных трем,
+# программа должна выводить слово «Fizz»
+# ✔ Вместо чисел, кратных пяти — слово «Buzz».
+# ✔ Если число кратно и 3, и 5, то программа
+# должна выводить слово «FizzBuzz».
+# ✔ *Превратите решение в генераторное выражение.
+
+
+def like_a_gen():
+    for i in range(0, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+def gen2():
+    for i in range(0, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            yield "FizzBuzz"
+        elif i % 3 == 0:
+            yield "Fizz"
+        elif i % 5 == 0:
+            yield "Buzz"
+        else:
+            yield i
+
+
+for elem in iter(gen2()):
+    print(elem)
